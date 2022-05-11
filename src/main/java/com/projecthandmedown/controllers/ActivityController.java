98c407f94ef744fd14549fd17b142c1aecc6055a
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-05-11 13:44:39 -0500

    fixed bug for searching

--- src/main/java/com/projecthandmedown/controllers/ActivityController.java
+++ src/main/java/com/projecthandmedown/controllers/ActivityController.java
@@ -26,7 +26,7 @@ public class ActivityController {
     private final EmailService emailService;
     private final ActivityCategoryRepository activityCatDao;
 
-    public ActivityController(ActivityRepository activityDao, UserRepository userDAO, EmailService emailService, ActivityCategoryRepository activityCatDao){
+    public ActivityController(ActivityRepository activityDao, UserRepository userDAO, EmailService emailService, ActivityCategoryRepository activityCatDao) {
         this.activityDao = activityDao;
         this.emailService = emailService;
         this.userDAO = userDAO;
@@ -34,32 +34,28 @@ public class ActivityController {
     }
 
     @GetMapping("/activities")
-    public String activitiesView(Model model){
+    public String activitiesView(Model model) {
 
-       List<Activity> activities =activityDao.findAll();
-       model.addAttribute("activities",activities);
+        List<Activity> activities = activityDao.findAll();
+        model.addAttribute("activities", activities);
 
         return "activities/activitiesView";
     }
 
     @GetMapping("/activities/{id}")
-    public String activityView(Model model, @PathVariable Long id){
+    public String activityView(Model model, @PathVariable Long id) {
         Activity activity = activityDao.getById(id);
-        model.addAttribute("activity",activity);
+        model.addAttribute("activity", activity);
         return "activities/activityView";
     }
 
     @GetMapping("/activities/categories/{id}")
-    public String viewByCategory(Model model, @PathVariable Long id){
-        List <ActivityCategory> categories = activityCatDao.findAll();
-       List <Activity> activities = activityCatDao.getById(id).getActivities();
+    public String viewByCategory(Model model, @PathVariable Long id) {
+        List<ActivityCategory> categories = activityCatDao.findAll();
+        List<Activity> activities = activityCatDao.getById(id).getActivities();
 
-       model.addAttribute("categories",categories);
-       model.addAttribute("activities",activities);
-
-
-
-
+        model.addAttribute("categories", categories);
+        model.addAttribute("activities", activities);
 
 
         return "activities/activitiesView";
@@ -70,24 +66,24 @@ public class ActivityController {
     private String filestackKey;
 
     @GetMapping("/activities/create")
-    public String createActivity(Model model){
+    public String createActivity(Model model) {
         model.addAttribute("activity", new Activity());
         model.addAttribute("filestackKey", filestackKey);
         List<ActivityCategory> categories = activityCatDao.findAll();
-        model.addAttribute("categories",categories);
+        model.addAttribute("categories", categories);
 
 
         return "activities/activityCreate";
     }
 
     @PostMapping("/activities/create")
-    public String addActivity(@ModelAttribute Activity activity,RedirectAttributes attr,Model model
-    ){
+    public String addActivity(@ModelAttribute Activity activity, RedirectAttributes attr, Model model
+    ) {
 
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         activity.setUser(user); // <-- this will be setting     user for post.
 
-        if(activity.getTitle().equals("") || activity.getBody().equals("")){
+        if (activity.getTitle().equals("") || activity.getBody().equals("")) {
             return "activities/activityCreate";
         }
 
@@ -95,10 +91,10 @@ public class ActivityController {
         activityDao.save(activity);
 
 
-        attr.addFlashAttribute("createMsg","Successfully added a new post");
+        attr.addFlashAttribute("createMsg", "Successfully added a new post");
 
 
-      emailService.prepareAndSendActivity(activity,activity.getTitle(),activity.getBody());
+        emailService.prepareAndSendActivity(activity, activity.getTitle(), activity.getBody());
 
 
         return "redirect:/activities";
@@ -106,67 +102,88 @@ public class ActivityController {
 
 
     @GetMapping("/activities/{id}/edit")
-    public String editPost(@PathVariable Long id, Model model){
+    public String editPost(@PathVariable Long id, Model model) {
         Activity activity = activityDao.getById(id);
 
         List<ActivityCategory> categories = activityCatDao.findAll();
 
 
-        model.addAttribute("activity",activity);
-        model.addAttribute("categories",categories);
+        model.addAttribute("activity", activity);
+        model.addAttribute("categories", categories);
 
         return "activities/activityEdit";
 
     }
 
     @PostMapping("activities/edit")
-    public String editAndSubmit(@ModelAttribute Activity activity){
+    public String editAndSubmit(@ModelAttribute Activity activity) {
 
 
         activityDao.save(activity);
         return "redirect:/activities";
     }
 
-    @GetMapping ("activities/{id}/delete")
-    public String deleteActivity(@PathVariable Long id,Model model, RedirectAttributes attr){
+    @GetMapping("activities/{id}/delete")
+    public String deleteActivity(@PathVariable Long id, Model model, RedirectAttributes attr) {
         Activity activity = activityDao.getById(id);
         activity.getActivityCategories().clear();
         activityDao.delete(activity);
-        attr.addFlashAttribute("deleteMsg","Successfully deleted the post");
+        attr.addFlashAttribute("deleteMsg", "Successfully deleted the post");
         return "redirect:/activities";
     }
 
     @GetMapping("activities/user/{user_id}")
-    public String seeAllUserPosts(@PathVariable Long user_id,Model model){
+    public String seeAllUserPosts(@PathVariable Long user_id, Model model) {
         User targetUser = userDAO.getUserById(user_id);
         List<Activity> activities = activityDao.getByUser(targetUser);
-        model.addAttribute("activities",activities);
+        model.addAttribute("activities", activities);
 
-        return"activities/activityUserPosts";
+        return "activities/activityUserPosts";
+    }
+
+
+//    checking for duplicate in List
+   public static List<Activity> checkDuplicate( List<Activity> lists){
+        ArrayList<Activity> arr = new ArrayList<>();
+        for (Activity element : lists){
+            if(!lists.contains(element)){
+                arr.add(element);
+            }
+        }
+        return arr;
     }
 
 
     @GetMapping("activities/search")
-    public String filteredActivities (Model model, @RequestParam String keyword) {
-
+    public String filteredActivities(Model model, @RequestParam String keyword) {
 
 
         model.addAttribute("keyword", keyword.toLowerCase(Locale.ROOT));
         List<Activity> activities = activityDao.findAll();
         List<Activity> filteredActivities = new ArrayList<>();
 
+
+
         for (int i = 0; i < activities.size(); i++) {
             Activity activity = activities.get(i);
             String title = activity.getTitle();
-            if (title.toLowerCase().contains(keyword.toLowerCase())) {
-                System.out.println(title);
-                filteredActivities.add(activity);
-            }
+            String body = activity.getBody();
 
+
+            if((title.toLowerCase().contains(keyword.toLowerCase())) || (body.toLowerCase().contains(keyword.toLowerCase()))){
+                filteredActivities.add(activity); // wil have duplicates.
+                checkDuplicate(filteredActivities);
+            }
         }
-        model.addAttribute("activities",filteredActivities);
+
+
+
+        model.addAttribute("activities", filteredActivities);
+
         return "activities/ActivityFiltered";
     }
+
+
 }
 
 
