46f77a4ea4d33ab9c2915f0495c97df66d06ed87
Adrian Brown <adrian.r.brown90@gmail.com>
2022-05-19 06:51:44 -0700

    feat(redirect): fixed forum edit post and edit reply redirect feature; back to post instead of forum

--- src/main/java/com/projecthandmedown/controllers/ForumController.java
+++ src/main/java/com/projecthandmedown/controllers/ForumController.java
@@ -130,7 +130,7 @@ public class ForumController {
     @PostMapping("/edit/post")
     public String edit(@ModelAttribute ForumPost post) {
         forumPostDao.save(post);
-        return "redirect:/forum";
+        return "redirect:/forum_post/" + post.getId();
     }
 
     @GetMapping("post/{id}/delete")
@@ -163,8 +163,7 @@ public class ForumController {
     @PostMapping("edit/reply")
     public String addEditedReply(@ModelAttribute ForumReply reply) {
         forumReplyDao.save(reply);
-        String redirect = "redirect:/forum_post/" + reply.getForumPost().getId();
-        return redirect;
+        return "redirect:/forum_post/" + reply.getForumPost().getId();
     }
 
     @GetMapping("reply/{id}/delete")
@@ -174,6 +173,4 @@ public class ForumController {
         forumReplyDao.delete(reply);
         return "redirect:/forum_post/" + post.getId();
     }
-
-    //TODO: enable deleting a comment and redirecting to the post source after deletion.
 }
